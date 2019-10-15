a3eea23a20a4a7207c446a8b6cf288cc38772007
W. Logan Downing <30451101+Foxhound013@users.noreply.github.com>
2019-10-15 14:52:29 -0400

    Update to polygon analytic functions

--- project4/WLoganDowning_P4.R
+++ project4/WLoganDowning_P4.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(sf)
 library(tmap)
 library(tmaptools)
+library(rgeos)
 
 
 # 1. Given a building shapefile of Purdue campus, make an R program to calculate 
@@ -38,32 +39,187 @@ tmp <- data$geometry[1][[1]][1]
 # accepts a dataframe but expects a geometry column
 ld_perimeter <- function(data) {
   perimeters <- list()
+
   for (i in data$geometry) {
-    y <- diff(i[[1]][,2])
-    x <- diff(i[[1]][,1])
-    
-    perimeters <- append(perimeters, sum(sqrt(y^2+x^2)))
+    # check polygon type
+    if (st_is(i,'POLYGON')) {
+      y <- diff(i[[1]][,2])
+      x <- diff(i[[1]][,1])
+      
+      perimeters <- append(perimeters, sum(sqrt(y^2+x^2)))
+    } else if (st_is(i,'MULTIPOLYGON')){
+      multi_perim <- 0
+      for (j in 1:length(i)) {
+        y <- diff(i[[j]][[1]][,2])
+        x <- diff(i[[j]][[1]][,1])
+        
+        multi_perim <- multi_perim + sum(sqrt(y^2+x^2))
+      }
+      #sum the multipolygon
+      perimeters <- append(perimeters, multi_perim)
+    }
   }
+  return(unlist(perimeters))
+}
+
+data$ld_perim <- ld_perimeter(data)
+summary(data$ld_perim-data$PERIMETER)
+# my method doesn't seem to be perfect. There are some strange edge cases but the 1st and 3rd
+# quartiles are fine. (compared to the inbuilt perimeters)
+
+
+# Area Calculation
+# Formula: 1/2 * sum( (xi+1-xi)*(yi+yi+1) )
+
+tmp <- data$geometry
+y <- head(tmp[[1]][[1]][,2], -1) + tail(tmp[[1]][[1]][,2], -1)
+x <- diff(tmp[[1]][[1]][,1])
+
+.5 * sum(y*x)
+st_area(data[1,])
+
+ld_area <- function(data) {
+  area <- list()
   
-  return(perimeters)
+  for (i in data$geometry) {
+    # check polygon type
+    if (st_is(i,'POLYGON')) {
+      y <- head(i[[1]][,2], -1) + tail(i[[1]][,2], -1)
+      x <- diff(i[[1]][,1])
+      
+      area <- append(area, .5 * sum(y*x))
+    } else if (st_is(i,'MULTIPOLYGON')) {
+      multi_area <- 0
+      for (j in 1:length(i)) {
+        y <- head(i[[j]][[1]][,2], -1) + tail(i[[j]][[1]][,2], -1)
+        x <- diff(i[[j]][[1]][,1])
+        multi_area <- multi_area + .5 * sum(y*x)
+      }
+      
+      area <- append(area, multi_area)
+    }
+  }
+  return(unlist(area))
 }
 
-# sapply(data$geometry, function(x) x[[1]])
+data$ld_area <- ld_area(data)
+summary(data$ld_area - data$Shape_Area)
+# everything appears to be good, there are some discrepancies but overall it's dead on.
 
-ld_perimeter(data)
+# Centroid Calculation
+# Formulas:
+#   x_bar = 1/(6A) * Sum( (yi+1 - yi) * (xi^2 + xi*xi+1 + xi+1^2) )
+#   y_bar = 1/(6A) * Sum( (xi - xi+1) * (yi^2 + yi*yi+1 + yi+1^2) )
 
-# this is failing on multipolygons ...
+#x_bar
+y <- diff(data$geometry[[1]][[1]][,2])
+x <- head(data$geometry[[1]][[1]][,1], -1)^2 + 
+  head(data$geometry[[1]][[1]][,1], -1)*tail(data$geometry[[1]][[1]][,1], -1) +
+  tail(data$geometry[[1]][[1]][,1], -1)^2
+  
+x_bar <- (1/(6*data$ld_area[1]) * sum(y*x))*-1
 
-# you can add an st_is check to determine if it is a multipolygon
 
-for (i in data$geometry) {
-  print(i[[1]][,1])
+x <- head(data$geometry[[1]][[1]][,1], -1) - tail(data$geometry[[1]][[1]][,1], -1)
+y <- head(data$geometry[[1]][[1]][,2], -1)^2 +
+  head(data$geometry[[1]][[1]][,2], -1)*tail(data$geometry[[1]][[1]][,2], -1) +
+  tail(data$geometry[[1]][[1]][,2], -1)^2
 
+y_bar <- (1/(6*data$ld_area[1]) * sum(y*x))*-1
+
+st_geometry(tmp) <- st_centroid(data[1,])
+plot(tmp$geometry)
+plot(st_centroid(tmp$geometry), add=T, pch=3)
+tmp$geometry
+pts <- do.call(rbind, st_geometry(data)) 
+# the above line is just to pull the centroid back out of the attributes
+
+rbind(st_geometry(tmp))
+
+# this function will return two columns of x and y centroid values
+# area should be calculated prior to running this function
+# requires a geometry column and area column. Area must be defined when called
+ld_centroid <- function(data, area) {
+  data <- data$geometry
+  
+  # Initialize a container to store centroid data
+  centroids <- data.frame(x_bar = rep(NA, length(data)),
+                          y_bar = rep(NA, length(data))) 
+  
+  for (i in 1:length(data)) {
+    # verify polygon type
+    if (st_is(data[[i]], 'POLYGON')) {
+      #x_bar
+      y <- diff(data[[i]][[1]][,2])
+      x <- head(data[[i]][[1]][,1], -1)^2 + 
+        head(data[[i]][[1]][,1], -1)*tail(data[[i]][[1]][,1], -1) +
+        tail(data[[i]][[1]][,1], -1)^2
+      
+      x_bar <- (1/(6*area[i]) * sum(y*x))*-1
+      
+      #y_bar
+      x <- head(data[[i]][[1]][,1], -1) - tail(data[[i]][[1]][,1], -1)
+      y <- head(data[[i]][[1]][,2], -1)^2 +
+        head(data[[i]][[1]][,2], -1)*tail(data[[i]][[1]][,2], -1) +
+        tail(data[[i]][[1]][,2], -1)^2
+      
+      y_bar <- (1/(6*area[i]) * sum(y*x))*-1
+      
+      centroids[i, ] <- rbind(x_bar, y_bar)
+    } else if (st_is(data[[i]], 'MULTIPOLYGON')) {
+      x_list <- rep(list(NULL),length(data[[i]]))
+      y_list <- rep(list(NULL),length(data[[i]]))
+      next()
+    #   for (j in 1:length(data[[i]])) {
+    #     #x_bar
+    #     y <- diff(data[[i]][[j]][[1]][,2])
+    #     x <- head(data[[i]][[j]][[1]][,1], -1)^2 + 
+    #       head(data[[i]][[j]][[1]][,1], -1)*tail(data[[i]][[j]][[1]][,1], -1) +
+    #       tail(data[[i]][[j]][[1]][,1], -1)^2
+    #     
+    #     x_bar <- (1/(6*area[i]) * sum(y*x))*-1# this bit isn't technically right.
+    #     # to fix it, it would require tweaking the area function to return the individual
+    #     # areas for each polygon
+    # 
+    #     #y_bar
+    #     x <- head(data[[i]][[j]][[1]][,1], -1) - tail(data[[i]][[j]][[1]][,1], -1)
+    #     y <- head(data[[i]][[j]][[1]][,2], -1)^2 +
+    #       head(data[[i]][[j]][[1]][,2], -1)*tail(data[[i]][[j]][[1]][,2], -1) +
+    #       tail(data[[i]][[j]][[1]][,2], -1)^2
+    #     
+    #     y_bar <- (1/(6*area[i]) * sum(y*x))*-1 # this bit isn't technically right.
+    #     # to fix it, it would require tweaking the area function to return the individual
+    #     # areas for each polygon
+    #     
+    #     # now, you need to store each multipoly's center to a list, for future storage in 
+    #     # the data frame.
+    #     x_list[[j]] <- append(x_list[[j]], x_bar)
+    #     y_list[[j]] <- append(y_list[[j]], y_bar)
+    #   }
+    #   x_list <- sapply(x_list, cbind)
+    #   y_list <- sapply(y_list, cbind)
+    #   
+    #   print(centroids[890:898,])
+    #   centroids[[1]][i][[1]] <- x_list
+    #   centroids[[2]][i][[1]] <- y_list
+    }
+  }
+  return(centroids)
 }
-i[[1]]
 
-i[[1]][,2]
-i[[1]][[1]]
+centroid_data <- ld_centroid(data, data$ld_area)
+data$ld_centroid_x <- centroid_data$x_bar
+data$ld_centroid_y <- centroid_data$y_bar
+
+
+# calculate perimeter with inbuilt function
+data$builtIn_perimeter <- st_length(st_cast(data, 'MULTILINESTRING'))
+data$builtIn_area <- st_area(data)
+pts <- data.frame(st_geometry(data) %>% st_centroid %>% st_coordinates)
+data$builtIn_centroidX <- pts$X
+data$builtIn_centroidY <- pts$Y
+
+
 
 # 2. Given the WL tweets data of year 2014, make an R program to (use built-in functions, 6pts)
 #
