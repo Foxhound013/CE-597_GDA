573ea94007523b8279512f15c835fe3708a86a81
W. Logan Downing <30451101+Foxhound013@users.noreply.github.com>
2019-10-31 17:30:42 -0400

    Closing in on completion - Making final changes and analysis.

--- project5/WLoganDowning_P5.R
+++ project5/WLoganDowning_P5.R
@@ -3,12 +3,15 @@ library(sf)
 library(tmap); tmap_mode('view')
 library(mapview)
 library(lattice)
+library(dplyr)
+library(exactextractr)
+library(rasterVis)
 
 # 1. Data
 #   1) DEM over entire Indiana (given)
 #   2) County boundaries of IN (to download)
 #   3) Flood plain of IN (to download)
-#   4) Land covey maps of the entire country (entire US, raster; to download)
+#   4) Land cover maps of the entire country (entire US, raster; to download)
 
 # Data has been download, this section will load these data sources
 dem <- raster('./data/raw/IN_DEM_UTM/indiana_utm84')
@@ -21,9 +24,9 @@ st_crs(counties)
 flood <- st_read('./data/raw/Floodplains_FIRM', layer='Floodplains_FIRM_IDNR_IN')
 st_crs(flood)
 
-nlcd <- brick('./data/raw/NLCD_2016_LandCover/NLCD_2016_Land_Cover_L48_20190424.img')
+nlcd <- raster('./data/raw/NLCD_2016_LandCover/NLCD_2016_Land_Cover_L48_20190424.img')
 nlcd; st_crs(nlcd); image(nlcd) # CONUS
-nlcd.data.att <- nlcd@data@attributes # capture the data attributes
+
 # 2. Terrain analysis (2 pts)
 #   1) Map the elevation
 #   2) Calculate and map the slope
@@ -37,12 +40,20 @@ tm_shape(dem) + tm_raster(alpha=0.7, title='Elevation') + tm_scale_bar() +
 slope <- terrain(dem, opt='slope')
 mapview(slope)
 
-hist(dem, main='Elevation', ylab='Frequency', xlab='Elevation (meters)')
-hist(slope, main='Slope', ylab='Frequency', xlab='Slope (degrees)')
-xyplot(slope[1000,]~dem[1000,]) # 1 row cross section
-xyplot(slope[,1000]~dem[,1000]) # 1 column cross section
+# coerce the rasters to vectors
+dem.vect <- as.data.frame(dem, xy=T) %>% na.omit
 
+slope.vect <- as.data.frame(slope, xy=T) %>% na.omit
 
+dem_slope <- cbind(sample(slope.vect$slope, 250000), sample(dem.vect$indiana_utm84, 250000)) %>%
+  as.data.frame
+colnames(dem_slope) <- c('slope', 'elevation')
+
+png('./figures/slopeVelevation.png')
+xyplot(slope~elevation, data=dem_slope, pch=16, col='deepskyblue3', alpha=0.4,
+       main='Slope Vs. Elevation for Indiana - Random 250K Samples', ylab='Slope (Degrees)',
+       xlab='Elevation (m)')
+dev.off()
 
 
 # 3. Evaluate the ‘zone’ data (2 pts)
@@ -51,27 +62,36 @@ xyplot(slope[,1000]~dem[,1000]) # 1 column cross section
 #   3) Prepare and map the flood maps
 #   4) Crop, clip or mask them when needed; make sure they are ready for next tasks.
 
+
 #1)
 counties <- st_transform(counties, crs=st_crs(dem))
+dem <- crop(dem, counties)
+mapview(dem)
 tm_shape(counties) + tm_polygons() + tm_scale_bar()
 
 
 #2) The nlcd data is way to big to try to map prior to subsetting, you'll need to clip it with other data first
 start <- Sys.time(); nlcd <- crop(nlcd, st_transform(counties, crs=st_crs(nlcd))); end <- Sys.time();
 end-start
-# Warning, projection takes roughly 16 minutes.
-start <- Sys.time(); nlcd <- projectRaster(nlcd, crs=st_crs(dem)$proj4string, method='ngb'); end <- Sys.time();
+# Warning, projection takes a very long time
+start <- Sys.time(); nlcd <- projectRaster(nlcd, crs=st_crs(dem)$proj4string, method='ngb', res=c(30,30)); end <- Sys.time();
 end-start;
 
 nlcd <- ratify(nlcd)
 nlcd@data@attributes[[1]]$NLCD.class <- c('Open Water', 'Developed, Open Space', 'Developed, Low Intensity', 'Developed, Medium Intensity', 'Developed, High Intensity', 'Barren Land (Rock/Sand/Clay)', 'Deciduous Forest', 'Evergreen Forest', 'Mixed Forest', 'Shurb/Scrub', 'Grassland/Herbaceous', 'Pasture/Hay', 'Cultivated Crops', 'Woody Wetlands', 'Emergent Herbaceous Wetlands')
 
 mapview(nlcd)
-
+myPallette = rev(c('#64B3D5', '#C8E6F8', '#CA9146', '#FBF65D', '#FDE9AA', '#DCCA8F', '#D4E7B0', 
+                   '#38814E', '#85C77E', '#D2CDC0', '#B50000', '#FF0000', '#E29E8C', '#E8D1D1',
+                   '#5475A8'))
+rasterVis::levelplot(nlcd, col.regions=myPallette, main='NLCD Raster for Indiana',
+                     xlab='Longitude (m)', ylab='Latitude (m)')
 
 #3) flood data takes forever to plot
 flood <- lwgeom::st_make_valid(flood) # not all of the geometries are valid
 flood <- st_transform(flood, crs=st_crs(dem))
+
+
 
 ##############################
 # ISSUE GETTING MAPPING FOR FLOOD
@@ -85,19 +105,61 @@ flood <- st_transform(flood, crs=st_crs(dem))
 #   2) Determine the mean, std. dev. of elevation and slope for land cover classes
 #   3) Determine the mean, standard deviation of elevation and slope for flood zones
 
+# Load transformed data from .RData file
+load('./data/processed/projectedData.RData')
+
 #1)
-elevMean.cnty <- raster::extract(dem, counties, method='simple', fun=mean, na.rm=T)
-# works but very slow (40 seconds for 1 county)
-start <- Sys.time()
-slopeMean.cnty <- raster::extract(slope, counties[1,], method='simple', fun='mean')
-end <- Sys.time(); end-start
+elevMean.cnty <- exact_extract(dem, counties, fun='mean')
+elevStd.cnty <- exact_extract(dem, counties, function(value, cov_frac) sd(value*cov_frac))
 
-#2)
-tmp <- nlcd
-extent(tmp) <- extent(dem)
-elevSlope.nlcd <- zonal(dem, tmp, fun='mean', na.rm=T)
+slopeMean.cnty <- exact_extract(slope, counties, fun='mean')
+slopeStd.cnty <- exact_extract(slope, counties, function(value, cov_frac) sd(value*cov_frac))
 
+counties$elevMean <- elevMean.cnty
+counties$elevStd <- elevStd.cnty
+counties$slopeMean <- slopeMean.cnty
+counties$slopeStd <- slopeStd.cnty
+
+tmap_mode('plot')
+
+tm_shape(counties) + tm_fill('elevMean', title='Elevation (m)') + tm_borders() + 
+  tm_scale_bar() + tm_layout()
+
+tm_shape(counties) + tm_fill('elevStd', title='Elevation (m)') + tm_borders() + 
+  tm_scale_bar() + tm_layout()
+
+tm_shape(counties) + tm_fill('slopeMean', title='Slope (Degrees)') + tm_borders() + 
+  tm_scale_bar() + tm_layout()
+
+tm_shape(counties) + tm_fill('slopeStd', title='Slope (Degrees)') + tm_borders() + 
+  tm_scale_bar() + tm_layout()
+
+#2) the data has to be coerced to match up
+nlcd.dem <- crop(nlcd, dem)
+extent(nlcd.dem) <- extent(dem)
+elevMean.nlcd <- zonal(dem, nlcd.dem, fun='mean', method='simple')
+elevStd.nlcd <- zonal(dem, nlcd.dem, fun=sd, method='simple')
+
+nlcd.slope <- crop(nlcd,slope)
+slope.nlcd <- crop(slope, nlcd.slope)
+extent(slope.nlcd) <- extent(nlcd.slope)
+slopeMean.nlcd <- zonal(slope.nlcd, nlcd.slope, fun='mean', method='simple')
+slopeStd.nlcd <- zonal(slope.nlcd, nlcd.slope, fun=sd, method='simple')
+
+# Data is fine being tabular.
+# save the data up to this point
+load('./data/processed/projected&processedData.Rdata')
+
+#3) ### Not quite sure what to do with these, the data values are entirely too high.
+elevMean.fld <- exact_extract(dem, flood, fun='mean')
+elevStd.fld <- exact_extract(dem, flood, function(value, cov_frac) sd(value*cov_frac))
+
+slopeMean.fld <- exact_extract(slope, flood, fun='mean')
+slopeStd.fld <- exact_extract(slope, flood, function(value, cov_frac) sd(value*cov_frac))
 # 5. Bonus (3 pts)
 #   1) Evaluate the county’s risk based on its population and its floodplain
 #   2) Reclassify/classify elevations (or slopes) using one of the data classifiers we discussed
 #   3) Implement your own slope calculator and evaluate it
+
+
+
