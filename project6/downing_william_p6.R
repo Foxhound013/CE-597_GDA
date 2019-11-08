97132a34a3145e2b4ddc054775665f7dd81e68cf
W. Logan Downing <30451101+Foxhound013@users.noreply.github.com>
2019-11-08 15:08:40 -0500

    Completion of Project 6

--- project6/downing_william_p6.R
+++ project6/downing_william_p6.R
@@ -1,13 +1,11 @@
 library(sf)
 library(dplyr)
 library(spdep)
-library(ggplot2)
 
 # 1. Data
 #   1) Indiana County Census data
 
 counties <- st_read('./data/raw/census_counties', layer='Census_County_TIGER00_IN')
-st_crs(counties)
 
 # 2. Spatial connectivity evaluation (1 pt)
 #   1) Plot the connection of counties under two different neighborhood definitions
@@ -33,6 +31,7 @@ dev.off()
 #   3) Show relevant plots and maps to support your discussions; discuss the spatial patterns
 #     you would conclude for POP2000’s distribution under the weight matrices
 
+
 # using cnxnx.r unless otherwise told to from this point forward.
 weight.r.ww <- nb2listw(cnxn.r, style='W') # default: row standardized. Sums over all links to n
 weight.r.wb <- nb2listw(cnxn.r, style='B') # Basic Binary
@@ -45,9 +44,11 @@ mi.r.ww <- moran.test(counties$POP2000, weight.r.ww, randomisation=F); mi.r.ww
 mi.r.wb <- moran.test(counties$POP2000, weight.r.wb, randomisation=F); mi.r.wb
 mi.r.wc <- moran.test(counties$POP2000, weight.r.wc, randomisation=F); mi.r.wc
 
-plot(counties['POP2000'], breaks='jenks', key.pos=1, main='Population by County for Year 2000',
-     axes=T)
-
+png('./figures/pop2000_countyJenks.png')
+plot(counties['POP2000'], breaks='jenks', key.pos=4, main='Population by County for Year 2000',
+     axes=T, xlab='Longitude (m)', ylab='Latitude (m)')
+dev.off()
+# maybe add a comparison of neighborhood weights
 
 png('./figures/moransI_ww.png')
 moran.plot(counties$POP2000, listw=weight.r.ww, main="Moran's I: Row Standardized",
@@ -73,24 +74,39 @@ dev.off()
 #     attributes, respectively
 
 # Looking at male vs female distribution
+png('./figures/males_map.png')
 plot(counties['MALES'], breaks='jenks', key.pos=1, main='Spatial Distribution of Males', axes=T)
+dev.off()
+png('./figures/females_map.png')
 plot(counties['FEMALES'], breaks='jenks', key.pos=1, main='Spatial Distribution of Females', axes=T)
+
 # Female population is smaller, especially in rural areas.
 
 # using weight.r.ww
-moran.test(counties$MALES, listw=weight.r.ww, randomisation=T)
+moran.test(counties$MALES, listw=weight.r.ww, randomisation=F)
+png('./figures/moran_test_males.png')
 moran.plot(counties$MALES, listw=weight.r.ww, main="Moran's I - Male Population - Row Standardized",
            xlab='Populations', ylab='Spatially Lagged Populations')
-moran.test(counties$FEMALES, listw=weight.r.ww, randomisation=T)
+dev.off()
+
+moran.test(counties$FEMALES, listw=weight.r.ww, randomisation=F)
+png('./figures/moran_test_females.png')
 moran.plot(counties$FEMALES, listw=weight.r.ww, main="Moran's I - Female Population - Row Standardized",
            xlab='Populations', ylab='Spatially Lagged Populations')
+dev.off()
 
-mc.male <- moran.mc(counties$MALES, listw=weight.r.ww, nsim=5000)
+
+mc.male <- moran.mc(counties$MALES, listw=weight.r.ww, nsim=5000); mc.male
+png('./figures/moran_mc_males.png')
 plot(mc.male, type='b', pch=16, col='deepskyblue3', xlab='Male Population', 
      ylim=c(0,10), xlim=c(-0.2, 0.4))
-mc.female <- moran.mc(counties$FEMALES, listw=weight.r.ww, nsim=5000)
+dev.off()
+
+mc.female <- moran.mc(counties$FEMALES, listw=weight.r.ww, nsim=5000); mc.female
+png('./figures/moran_mc_females.png')
 plot(mc.female, type='b', pch=16, col='deepskyblue3', xlab='Female Population', 
      ylim=c(0,10), xlim=c(-0.2, 0.4))
+dev.off()
 
 # 5. Local Moran’s I evaluation (3 pts)
 #   1) Use the same two attributes as above
@@ -103,12 +119,20 @@ localm.male <- localmoran(counties$MALES, listw=weight.r.ww)
 localm.female <- localmoran(counties$FEMALES, listw=weight.r.ww)
 
 pop.male <- cbind(counties, localm.male)
-hist(pop.male$Ii)
-plot(pop.male['Ii'], breaks='equal', key.pos=1, main="Local Moran's I - Male Population", axes=T)
+png('./figures/male_hist.png')
+hist(pop.male$Ii, main="Local Moran's I for the Male Population", xlab="Local i")
+dev.off()
+png('./figures/male_local_i.png')
+plot(pop.male['Ii'], breaks='jenks', key.pos=1, main="Local Moran's I - Male Population", axes=T)
+dev.off()
 
 pop.female <- cbind(counties, localm.female)
-hist(pop.female$Ii)
-plot(pop.female['Ii'], breaks='equal', key.pos=1, main="Local Moran's I - Female Population", axes=T)
+png('./figures/female_hist.png')
+hist(pop.female$Ii, main="Local Moran's I for the Female Population", xlab="Local i")
+dev.off()
+png('./figures/female_local_i.png')
+plot(pop.female['Ii'], breaks='jenks', key.pos=1, main="Local Moran's I - Female Population", axes=T)
+dev.off()
 
 # 6. Bonus (2 pts; 1 day after due)
 #   1) Make a function to map the HH,LL, HL,LH counties
